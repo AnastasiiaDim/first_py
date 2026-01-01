6593f93e83894b9bd13df312b1c8fff66449c9ac
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-01-01 16:41:50 +0100

    comment

--- lesson6_1.py
+++ lesson6_1.py
@@ -5,17 +5,16 @@
 # NOT — Reverses the condition
 
 # Task 1
-age = int(input("Please enter your age: "))
-user_answer = input("Do you have an ID? (yes/no): ").lower()
-has_id = (user_answer == "yes")
-
-if age >= 18 and has_id == True:
-    print("Access granted")
-else:
-    print("Access denied")
+# age = int(input("Please enter your age: "))
+# user_answer = input("Do you have an ID? (yes/no): ").lower()
+# has_id = (user_answer == "yes")
+#
+# if age >= 18 and has_id == True:
+#     print("Access granted")
+# else:
+#     print("Access denied")
 
 # Task 2
-#
 # admin = input("Are you an administrator? (yes/no): ").lower()
 # is_admin = admin == "yes"
 # ticket = input("Do you have a ticket (yes/no): ").lower()
@@ -27,7 +26,6 @@ else:
 #     print("Access denied")
 
 # Task 3
-
 # user_answer = input("Is your account blocked (yes/no): ").lower()
 # is_blocked = user_answer == "yes"
 #
