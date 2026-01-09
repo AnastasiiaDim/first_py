37ec841cbf1b9364bd72e3b9710dc380ed71a2d4
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-01-09 18:53:31 +0100

    feat: solved tasks in Functions

---
+++ Functions/solution_task6.py
@@ -0,0 +1,12 @@
+# Write a function that:
+# Receives two numbers
+# Returns their sum
+# Do NOT use print() inside the function
+# Then:
+# Call the function
+# Print the result outside
+
+def add(a,b):
+    return a+b
+
+print(add(10, 8))
