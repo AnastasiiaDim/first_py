0489fbc3e58a714f731f722bb003438518e81f56
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-02-12 16:56:00 +0100

    feat: task in System Design Practice

---
+++ system_design_practice/solution_task3.py
@@ -0,0 +1,31 @@
+CONFIG = (
+    ("Title", "My Awesome App"),
+    ("Version", 3.14),
+    ("Debug", True),
+    ("Max_Users", 100)
+)
+
+def get_config_value(target_key):
+    for name, value in CONFIG:
+        if name == target_key:
+            return value
+    return None
+
+def print_config():
+    print("--- Current Configuration ---")
+    for name, value in CONFIG:
+        print(f"Setting: {name} | Value: {value}")
+    print("-----------------------------")
+
+
+# USERS_DATA = (
+#     ("admin", "London", "Active"),
+#     ("ana", "Paris", "Away"),
+#     ("ivan", "Moscow", "Banned")
+# )
+#
+# def get_user_status(username):
+#     for name, city, status in USERS_DATA:
+#         if username == name:
+#             return status
+#     return None
