afcc9ad5c5249d121e11dfffb3206d8c3ddd20e2
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-03-12 12:08:25 +0100

    learned: syntax and basics of OOP

---
+++ OOP/study/login_system.py
@@ -0,0 +1,25 @@
+class User:
+    def __init__(self, username, age, role, password):
+        self.username = username
+        self.age = age
+        self.role = role
+        self.password = password
+
+class AuthSystem:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, user):
+        self.users[user.username] = user
+
+    def login(self, username, password):
+        if username not in self.users:
+            return False
+        return self.users[username].password == password
+
+system = AuthSystem()
+
+system.add_user(User("ana", 21, "user", "python"))
+system.add_user(User("admin", 30, "admin", "system"))
+
+print(system.login("ana", "python"))
