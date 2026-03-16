641f7159dc08d816efa2b270881aff289ea8d29f
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-03-16 21:21:18 +0100

    Car Class OOP practice

---
+++ OOP/study/car.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, brand, model, color):
+        self.brand = brand
+        self.model = model
+        self.color = color
+
+    def drive(self):
+        return f"Driving {self.brand} {self.model} in {self.color} color"
+
+car1 = Car("BMW", "Ford", "green")
+print(car1.drive())
