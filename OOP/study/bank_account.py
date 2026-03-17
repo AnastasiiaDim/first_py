ba066b246e952deb677ceb04ff1b4aa3f81d20ae
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-03-17 09:42:36 +0100

    simple Bank Account in OOP

---
+++ OOP/study/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance #Private attribute (double underscore) - Encapsulation
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if amount > 0:
+            self.__balance -= amount
+            return True
+        return False
+
+    def get_balance(self):
+        return self.__balance
+
+account_1 = BankAccount("", 100)
+account_1.deposit(10)
+account_1.withdraw(5)
+print(account_1.get_balance())
