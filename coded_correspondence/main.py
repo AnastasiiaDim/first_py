36f0cb0f7dade92ec918cda4573668a0c3fbbfae
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-03-24 18:00:46 +0100

    add: Ceasar Cipher

--- coded_correspondence/main.py
+++ coded_correspondence/main.py
@@ -14,4 +14,10 @@ def caesar_encode(message, offset):
     return caesar_decode(message, -offset)
 
 print(caesar_decode("hello!", 3))
-print(caesar_encode("bqdradyuzs ygxfubxq omqemd oubtqde fa oapq kagd yqeemsqe ue qhqz yadq eqogdq!", 14))
+
+# NO OFFSET GIVEN
+coded_message = "vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx."
+for i in range(1, 26):
+    candidate_message = caesar_decode(coded_message, i)
+    if "the" in candidate_message or "and" in candidate_message:
+        print(f"Offset {i}: {candidate_message}")
